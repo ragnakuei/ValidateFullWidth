60852f3a1c3d25b5f11d14c27b0548d08a5910ba
Kuei <ragnakuei@livemail.tw>
2021-02-25 09:11:40 +0800

    Add project files.

---
+++ ValidateFullWidth/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace ValidateFullWidth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblContainsFullWidthChar = new System.Windows.Forms.Label();
+            this.lblOutput2 = new System.Windows.Forms.Label();
+            this.lblContainsFullWidthChar2 = new System.Windows.Forms.Label();
+            this.lblOutput3 = new System.Windows.Forms.Label();
+            this.lblContainsFullWidthChar3 = new System.Windows.Forms.Label();
+            this.lblOutput4 = new System.Windows.Forms.Label();
+            this.lblContainsFullWidthChar4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(12, 12);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(148, 22);
+            this.tbxInput.TabIndex = 0;
+            this.tbxInput.TextChanged += new System.EventHandler(this.tbxInput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "轉成半型：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "是否包含全型字：";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(119, 48);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(37, 12);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = "Output";
+            // 
+            // lblContainsFullWidthChar
+            // 
+            this.lblContainsFullWidthChar.AutoSize = true;
+            this.lblContainsFullWidthChar.Location = new System.Drawing.Point(119, 76);
+            this.lblContainsFullWidthChar.Name = "lblContainsFullWidthChar";
+            this.lblContainsFullWidthChar.Size = new System.Drawing.Size(22, 12);
+            this.lblContainsFullWidthChar.TabIndex = 1;
+            this.lblContainsFullWidthChar.Text = "Yes";
+            // 
+            // lblOutput2
+            // 
+            this.lblOutput2.AutoSize = true;
+            this.lblOutput2.Location = new System.Drawing.Point(181, 48);
+            this.lblOutput2.Name = "lblOutput2";
+            this.lblOutput2.Size = new System.Drawing.Size(37, 12);
+            this.lblOutput2.TabIndex = 1;
+            this.lblOutput2.Text = "Output";
+            // 
+            // lblContainsFullWidthChar2
+            // 
+            this.lblContainsFullWidthChar2.AutoSize = true;
+            this.lblContainsFullWidthChar2.Location = new System.Drawing.Point(181, 76);
+            this.lblContainsFullWidthChar2.Name = "lblContainsFullWidthChar2";
+            this.lblContainsFullWidthChar2.Size = new System.Drawing.Size(22, 12);
+            this.lblContainsFullWidthChar2.TabIndex = 1;
+            this.lblContainsFullWidthChar2.Text = "Yes";
+            // 
+            // lblOutput3
+            // 
+            this.lblOutput3.AutoSize = true;
+            this.lblOutput3.Location = new System.Drawing.Point(246, 48);
+            this.lblOutput3.Name = "lblOutput3";
+            this.lblOutput3.Size = new System.Drawing.Size(37, 12);
+            this.lblOutput3.TabIndex = 1;
+            this.lblOutput3.Text = "Output";
+            // 
+            // lblContainsFullWidthChar3
+            // 
+            this.lblContainsFullWidthChar3.AutoSize = true;
+            this.lblContainsFullWidthChar3.Location = new System.Drawing.Point(246, 76);
+            this.lblContainsFullWidthChar3.Name = "lblContainsFullWidthChar3";
+            this.lblContainsFullWidthChar3.Size = new System.Drawing.Size(22, 12);
+            this.lblContainsFullWidthChar3.TabIndex = 1;
+            this.lblContainsFullWidthChar3.Text = "Yes";
+            // 
+            // lblOutput4
+            // 
+            this.lblOutput4.AutoSize = true;
+            this.lblOutput4.Location = new System.Drawing.Point(313, 48);
+            this.lblOutput4.Name = "lblOutput4";
+            this.lblOutput4.Size = new System.Drawing.Size(37, 12);
+            this.lblOutput4.TabIndex = 1;
+            this.lblOutput4.Text = "Output";
+            // 
+            // lblContainsFullWidthChar4
+            // 
+            this.lblContainsFullWidthChar4.AutoSize = true;
+            this.lblContainsFullWidthChar4.Location = new System.Drawing.Point(313, 76);
+            this.lblContainsFullWidthChar4.Name = "lblContainsFullWidthChar4";
+            this.lblContainsFullWidthChar4.Size = new System.Drawing.Size(22, 12);
+            this.lblContainsFullWidthChar4.TabIndex = 1;
+            this.lblContainsFullWidthChar4.Text = "Yes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 116);
+            this.Controls.Add(this.lblContainsFullWidthChar4);
+            this.Controls.Add(this.lblContainsFullWidthChar3);
+            this.Controls.Add(this.lblContainsFullWidthChar2);
+            this.Controls.Add(this.lblContainsFullWidthChar);
+            this.Controls.Add(this.lblOutput4);
+            this.Controls.Add(this.lblOutput3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblOutput2);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label lblOutput4;
+        private System.Windows.Forms.Label lblContainsFullWidthChar4;
+
+        private System.Windows.Forms.Label lblOutput3;
+        private System.Windows.Forms.Label lblContainsFullWidthChar3;
+
+        private System.Windows.Forms.Label lblContainsFullWidthChar2;
+        private System.Windows.Forms.Label lblOutput2;
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblContainsFullWidthChar;
+    }
+}
+
---
+++ ValidateFullWidth/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ValidateFullWidth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblOutput.Text                 = string.Empty;
+            lblContainsFullWidthChar.Text  = string.Empty;
+
+            lblOutput2.Text                = string.Empty;
+            lblContainsFullWidthChar2.Text = string.Empty;
+            
+            lblOutput3.Text                = string.Empty;
+            lblContainsFullWidthChar3.Text = string.Empty;
+            lblOutput4.Text                = string.Empty;
+
+            lblContainsFullWidthChar4.Text = string.Empty;
+        }
+
+        private void tbxInput_TextChanged(object sender, EventArgs e)
+        {
+            var targetTbx = sender as TextBox;
+
+            var inputText = targetTbx.Text;
+
+            var outputText = inputText.ToHalfWidth();
+
+            lblOutput.Text = outputText;
+            lblContainsFullWidthChar.Text = (inputText != outputText).ToString();
+
+            var outputText2 = inputText.ToHalfWidth2();
+
+            lblOutput2.Text = outputText2;
+            lblContainsFullWidthChar2.Text = (inputText != outputText2).ToString();
+
+            var outputText3 = inputText.ToHalfWidth3();
+
+            lblOutput3.Text = outputText3;
+            lblContainsFullWidthChar3.Text = (inputText != outputText3).ToString();
+
+            lblContainsFullWidthChar4.Text = inputText.IsContainsFullWidthChar().ToString();
+        }
+    }
+
+    public static class CommonHelper
+    {
+        public static string ToHalfWidth(this string source)
+        {
+            return Strings.StrConv(source, VbStrConv.Narrow, 0);
+        }
+
+        public static string ToHalfWidth2(this string source)
+        {
+            return source.Normalize(NormalizationForm.FormKC);
+        }
+
+        public static bool IsContainsFullWidthChar(this string source)
+        {
+            bool res = source.Any(c => c < 65375 && c > 65280);
+            if (res) {
+                // MessageBox.Show(string.Format("{0} => 含有全形字", str));
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public static class StringWidthHelper
+    {
+        private const uint LOCALE_SYSTEM_DEFAULT = 0x0800;
+        private const uint LCMAP_HALFWIDTH       = 0x00400000;
+        private const uint LCMAP_FULLWIDTH       = 0x00800000;
+
+        public static string ToHalfWidth3(this string fullWidth)
+        {
+            StringBuilder sb = new StringBuilder(256);
+            LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_HALFWIDTH, fullWidth, -1, sb, sb.Capacity);
+            return sb.ToString();
+        }
+
+        public static string ToFullWidth3(this string halfWidth)
+        {
+            StringBuilder sb = new StringBuilder(256);
+            LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_FULLWIDTH, halfWidth, -1, sb, sb.Capacity);
+            return sb.ToString();
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        private static extern int LCMapString(uint Locale, uint dwMapFlags, string lpSrcStr, int cchSrc, StringBuilder lpDestStr, int cchDest);
+    }
+}
